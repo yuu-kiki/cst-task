a76fe982801802b165ae67ad4f792ca0fa7113c3
yokoyama yuu <yuu@yokoyamanoMacBook-Pro.local>
2020-06-12 16:10:14 +0900

    if文を用いたプログラム

---
+++ if.py
@@ -0,0 +1,12 @@
+#xとyの数値の比較をする
+x = int (input("Xの数値を入力してください。\n")):
+y = int (input("yの数値を入力してください。\n")):
+
+if x > y:
+        print("xはyより大きい\n")
+
+elif x < y:
+        priint("xはyより小さい\n")
+
+else:
+        print("xとyは等しい\n")
